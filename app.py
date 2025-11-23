4a4944db369399a595e7e0def753a20fa12e570d
Lillian Fults <lillianfults@usf.edu>
2025-11-23 13:33:57 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-print ("Welcome to my Python program!")
-hours = input("How many hours did you study today? ")
-hours = float(hours)
-weekly_hours = hours * 7
-print(f"You are on track to study {weekly_hours} hours this week.")
-try:
+print ("Welcome to my Python program!") #welcome message
+hours = input("How many hours did you study today? ") #enter # of hours input
+hours = float(hours) #converts hours to flaot value
+weekly_hours = hours * 7 #multiply hours by 7 so can get weekly hours
+print(f"You are on track to study {weekly_hours} hours this week.") #display weekly hours using f-string
+try:#error handling if incorrect input value type
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
